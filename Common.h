5b7c2cc9b81b0ed21055749d5546749cb86a0c99
Madman10K <stanv.contact176@gmail.com>
2024-09-15 15:08:35 +0300

    Compilation error fix

--- Common.h
+++ Common.h
@@ -21,6 +21,14 @@ extern "C"
     #define MLS_PUBLIC_API
 #endif
 
+    typedef uint32_t UImGui_TextUtils_Colour;
+
+#define UIMGUI_COL32_R_SHIFT    0
+#define UIMGUI_COL32_G_SHIFT    8
+#define UIMGUI_COL32_B_SHIFT    16
+#define UIMGUI_COL32_A_SHIFT    24
+#define UIMGUI_COL32(R,G,B,A) (((UImGui_TextUtils_Colour)(A)<<UIMGUI_COL32_A_SHIFT) | ((UImGui_TextUtils_Colour)(B)<<UIMGUI_COL32_B_SHIFT) | ((UImGui_TextUtils_Colour)(G)<<UIMGUI_COL32_G_SHIFT) | ((UImGui_TextUtils_Colour)(R)<<UIMGUI_COL32_R_SHIFT))
+
     enum [[maybe_unused]] UImGui_TextUtils_WidgetState
     {
         UIMGUI_TEXT_UTILS_WIDGET_STATE_NONE = 0,
@@ -29,19 +37,18 @@ extern "C"
         UIMGUI_TEXT_UTILS_WIDGET_STATE_ALL = UIMGUI_TEXT_UTILS_WIDGET_STATE_CLICKED | UIMGUI_TEXT_UTILS_WIDGET_STATE_HOVERED,
     };
 
-    typedef uint32_t UImGui_TextUtils_Colour;
 
     // The default colour for links that have not been visited
-    #define UIMGUI_LINK_TEXT_UNVISITED IM_COL32(0, 0, 238, 255)
+    #define UIMGUI_LINK_TEXT_UNVISITED UIMGUI_COL32(0, 0, 238, 255)
 
     // The default colour for links that have been visited
-    #define UIMGUI_LINK_TEXT_VISITED IM_COL32(85, 26, 139, 255)
+    #define UIMGUI_LINK_TEXT_VISITED UIMGUI_COL32(85, 26, 139, 255)
 
     // The default highlight text colour, yellow with ~25% opacity
-    #define UIMGUI_HIGHLIGHT_TEXT_COLOUR IM_COL32(255, 255, 0, 64)
+    #define UIMGUI_HIGHLIGHT_TEXT_COLOUR UIMGUI_COL32(255, 255, 0, 64)
 
     // The default colour for blockquote rectangle, gray
-    #define UIMGUI_BLOCKQUOTE_TEXT_COLOUR IM_COL32(69, 71, 90, 255)
+    #define UIMGUI_BLOCKQUOTE_TEXT_COLOUR UIMGUI_COL32(69, 71, 90, 255)
 
 #ifdef __cplusplus
 }
